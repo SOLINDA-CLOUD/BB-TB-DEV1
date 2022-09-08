55225d17fdcbc2b7d06fd6f62138bdd601af292c
hamsky13 <ilham.ius13@gmail.com>
2022-09-08 19:25:38 +0700

    update

--- sol_bb_product/__manifest__.py
+++ sol_bb_product/__manifest__.py
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/product_views.xml',
+        'views/product_template_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
