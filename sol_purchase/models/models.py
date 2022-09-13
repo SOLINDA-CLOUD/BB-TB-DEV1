538efb3766cd5c3889f7cd2f56f310f32f7a3bee
inicoding <irfannurrahmat@outlook.com>
2022-09-13 16:40:26 +0700

    updat purchase

---
+++ sol_purchase/models/models.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class PurchaseRequestLine(models.Model):
+    _inherit = 'purchase.request.line'
+
+    image = fields.Image(string='Fabric Swatch')
+
+    @api.onchange('product_id')
+    def _onchange_image(self):
+        if self.product_id:
+            self.image = ''
+            if self.product_id.image_1920:
+                self.image = self.product_id.image_1920
+            self.image = self.image
+
+class PurchaseRequest(models.Model):
+    _inherit = 'purchase.request'
+
+    style_name = fields.Char(string='Style Name')
+    department = fields.Char(string='Department')
+    sub_department = fields.Char(string='Sub Department')
+    request_detail_id = fields.Many2one(string='Original Sample', comodel_name='request.detail', ondelete='cascade')
+    notes = fields.Html(string='Fit Notes')
+    name = fields.Char(string="Request Reference")
+
+    @api.model
+    def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].next_by_code('purchase.request.seq')
+        return super(PurchaseRequest, self).create(vals)
+
+class RequestDetail(models.Model):
+    _name = 'request.detail'
+    _description = 'Request Detail'
+
+    name = fields.Char(string='Original Sample')
+    sample_size = fields.Char(string='Sample Size')
+    approved_size = fields.Char(string='Sample In Approved Size')
+    sample_in_size = fields.Char(string='Please Make Sample In Size')
+    fabric = fields.Char(string='Fabric')
+    lining = fields.Char(string='Lining')
+    
