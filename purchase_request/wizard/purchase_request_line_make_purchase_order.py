228c4572fa2a167c7368ee989646948806a6b74c
inicoding <irfannurrahmat@outlook.com>
2022-09-25 20:38:30 +0700

    change button rfq to po

--- purchase_request/wizard/purchase_request_line_make_purchase_order.py
+++ purchase_request/wizard/purchase_request_line_make_purchase_order.py
@@ -48,15 +48,15 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
         company_id = False
 
         for line in self.env["purchase.request.line"].browse(request_line_ids):
-            if line.request_id.state == "done":
-                raise UserError(_("The purchase has already been completed."))
+            # if line.request_id.state == "done":
+            #     raise UserError(_("The purchase has already been completed."))
             if line.request_id.state != "approved":
                 raise UserError(
                     _("Purchase Request %s is not approved") % line.request_id.name
                 )
 
-            if line.purchase_state == "done":
-                raise UserError(_("The purchase has already been completed."))
+            # if line.purchase_state == "done":
+            #     raise UserError(_("The purchase has already been completed."))
 
             line_company_id = line.company_id and line.company_id.id or False
             if company_id is not False and line_company_id != company_id:
