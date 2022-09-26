abd1a4efd64ab67a2a87ae32bd6777c333956aca
inicoding <irfannurrahmat@outlook.com>
2022-09-26 16:16:07 +0700

    check error

--- sol_purchase/__manifest__.py
+++ sol_purchase/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'purchase_request', 'purchase', 'stock'],
+    'depends': ['base', 'purchase_request', 'purchase', 'stock', 'purchase_stock'],
 
     # always loaded
     'data': [
