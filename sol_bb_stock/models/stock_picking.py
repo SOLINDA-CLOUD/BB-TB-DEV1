4ff2a400cd3c8a40bbc9fa59dca214bbe09417d1
hamsky13 <ilham.ius13@gmail.com>
2022-09-29 12:00:06 +0700

    analytic account

--- sol_bb_stock/models/stock_picking.py
+++ sol_bb_stock/models/stock_picking.py
@@ -8,3 +8,15 @@ class StockPicking(models.Model):
         store=True,
         string='Analytic Account',
         readonly=False)
+
+    @api.depends('partner_id', 'date')
+    def _compute_analytic_account_id(self):
+        for order in self:
+            if not order.analytic_account_id:
+                default_analytic_account = order.env['account.analytic.default'].sudo().account_get(
+                    partner_id=order.partner_id.id,
+                    user_id=order.env.uid,
+                    date=order.date,
+                    company_id=order.company_id.id,
+                )
+                order.analytic_account_id = default_analytic_account.analytic_id
